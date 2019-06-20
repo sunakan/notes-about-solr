54887b60dd8e67ec4a978123d622cbfbf3fbabf6
sunakan <sunakan03@gmail.com>
2019-06-20 20:47:33 +0900

    [Add] zk, solrbのprintデバッグ勉強

--- containers/client/app/controllers/solr_api/v1_controller.rb
+++ containers/client/app/controllers/solr_api/v1_controller.rb
@@ -1,5 +1,17 @@
 class SolrApi::V1Controller < ApplicationController
   def select
+    collection_name  = "ch08_solrcloud_cluster"
+    collection_state = zk.get_collection_state(collection_name)
+    puts "====================="
+    pp collection_state
+    puts "====================="
     render json: {a: "ok"}
+  end
+
+  private
+  def zk
+    require "solr"
+    zookeeper_url = "zoo1:2181,zoo2:2181,zoo3:2181/solr"
+    @@zk ||= Solr::Cloud::ZookeeperConnection.new(zookeeper_url: zookeeper_url)
   end
 end
