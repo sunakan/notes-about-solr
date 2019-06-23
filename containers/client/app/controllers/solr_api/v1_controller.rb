ee95fce5c5ea0185dcde6382dce985319a3e4581
sunakan <sunakan03@gmail.com>
2019-06-23 17:55:05 +0900

    [Fix] ENVを利用していない箇所があったので利用する

--- containers/client/app/controllers/solr_api/v1_controller.rb
+++ containers/client/app/controllers/solr_api/v1_controller.rb
@@ -2,20 +2,24 @@
 
 class SolrApi::V1Controller < ApplicationController
   def select
-    url        = "#{target_node}/ch08_solrcloud_cluster/select"
+    url        = "#{target_node}/#{collection_name}/select"
+    puts url
     solr_query = params.permit!.to_h.except(:action, :controller)
-    response = JSON.parse(Faraday.get(url, solr_query).body)
+    response   = JSON.parse(Faraday.get(url, solr_query).body)
     render json: response
   end
 
   private
 
   def target_node
-    collection_name = ENV['COLLECTION_NAME']
     nodes       = Solr.active_nodes_for(collection: collection_name)
     shards      = Solr.shards_for(collection: collection_name)
     leaders     = shards.map { |shard| Solr.leader_replica_node_for(collection: collection_name, shard: shard) }
     non_leaders = nodes.select { |node| leaders.exclude?(node) }
     target_node = non_leaders.sample || leaders.sample
+  end
+
+  def collection_name
+    ENV['COLLECTION_NAME']
   end
 end
